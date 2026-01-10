feb86fd526a16b2e843cad2da0152feea34f266b
Vibha Vasisht <vibhavasisht09@gmail.com>
2026-01-10 18:11:43 +0530

    refactor(user): delete redundant user creation endpoints

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,9 @@
 import express from 'express';
 import User from '../models/User.js';
-import { createUser, getUsers, getUserById, updateUser, deleteUser } from '../controllers/user.controller.js';
+import { updateUser, deleteUser } from '../controllers/user.controller.js';
 import {protect} from '../middleware/auth.middleware.js';
 
 const router = express.Router();
-router.post('/', protect, createUser);
-router.get('/', protect, getUsers);
-router.get('/:id', protect, getUserById);
 router.put('/:id', protect, updateUser);
 router.delete('/:id', protect, deleteUser);
 export default router;
